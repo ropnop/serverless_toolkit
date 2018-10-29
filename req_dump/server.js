cc79e067ce35c4dec757a169a15e12e5aacc81b5
Ronnie Flathers <rflathers@gmail.com>
2018-10-28 22:10:50 -0500

    added req_dump

---
+++ req_dump/server.js
@@ -0,0 +1,44 @@
+const _ = require('lodash');
+const rawBody = require('raw-body');
+const requestIp = require('request-ip');
+
+// These are all added by CF and Zeit, I usually ignore them
+const IGNORED_HEADERS = [
+  "x-forwarded-host",
+  "x-real-ip",
+  "x-forwarded-for",
+  "x-forwarded-proto",
+  "x-now-id",
+  "x-now-log-id",
+  "x-zeit-co-forwarded-for",
+  "cf-ipcountry",
+  "cf-ray",
+  "cf-visitor",
+  "cf-connecting-ip",
+  "true-client-ip",
+]
+const IGNORE_HEADERS = true; // toggle this to show them
+
+
+module.exports = async (req, res) => {
+  const clientIp = requestIp.getClientIp(req);
+  let headers = {};
+  if (IGNORE_HEADERS) {
+    headers = _.reduce(req.headers, (headers, headerval, headername) => {
+      if (_.includes(IGNORED_HEADERS, headername)) {
+        return headers;
+      } else {
+        headers[headername] = headerval;
+        return headers;
+      }
+    }, {} );
+  } else { headers = req.headers; }
+  const data = {
+    url: req.url,
+    method: req.method,
+    headers: headers,
+    body: String(await rawBody(req)),
+    ip: clientIp
+  };
+  return data;
+}
